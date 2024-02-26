c6342cdcdfae446128b8fcb410e9aa3909c4954c
ratrikhanam000 <ratrikhanam000@gmail.com>
2024-02-26 11:35:03 +0600

    complete

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db("productDB").collection("product");
     //data receive from client side and store mongoDB
@@ -64,6 +64,13 @@ async function run() {
       const result = await productCollection.find(query).toArray();
       res.send(result);
     });
+    //for delete operation
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -78,7 +85,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("MAC Cosmetics");
 });
 
 app.listen(port, () => {
